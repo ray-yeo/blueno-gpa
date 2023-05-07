753f229529c474482dedfb42e8b8ea0456593de8
ray-yeo <raymondyeo25@gmail.com>
2023-05-07 13:33:12 -0400

    scroll

--- script.js
+++ script.js
@@ -129,10 +129,37 @@ function calculateGPA() {
         console.log(courseGrade)
 
         processCourseGrade();
+        const targetPosition = document.body.scrollHeight - window.innerHeight;
+        const startPosition = window.scrollY;
+        const distance = targetPosition - startPosition;
+        const duration = 1000; // in milliseconds
+        let start = null;
+      
+        function animation(currentTime) {
+          if (!start) {
+            start = currentTime;
+          }
+          const timeElapsed = currentTime - start;
+          const run = ease(timeElapsed, startPosition, distance, duration);
+          window.scrollTo(0, run);
+          if (timeElapsed < duration) {
+            requestAnimationFrame(animation);
+          }
+        }
+      
+        function ease(t, b, c, d) {
+          t /= d / 2;
+          if (t < 1) {
+            return c / 2 * t * t + b;
+          }
+          t--;
+          return -c / 2 * (t * (t - 2) - 1) + b;
+        }
+      
+        requestAnimationFrame(animation);
     };
     reader.readAsText(uploadedFile);
 
-    window.scrollTo(0, document.body.scrollHeight);
 
 }
 
